6b604863f446b586c0c4ae527a5c34e5d8474541
Ruslan Zamalov <zamruma@gmail.com>
2021-04-16 02:04:48 +0300

    Updates

---
+++ precache-manifest.e0a0fd4e5a78b68f37479bfc20e4361e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea905c9d015cc831edf6ca49a66a473d",
+    "url": "/github-pages-react-app/index.html"
+  },
+  {
+    "revision": "2ea7025b92949f3dc198",
+    "url": "/github-pages-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fa86183a0f5b51e26fa",
+    "url": "/github-pages-react-app/static/js/2.9cbbe798.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/github-pages-react-app/static/js/2.9cbbe798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea7025b92949f3dc198",
+    "url": "/github-pages-react-app/static/js/main.4ac2ff35.chunk.js"
+  },
+  {
+    "revision": "f0ad2746b60293ff422a",
+    "url": "/github-pages-react-app/static/js/runtime-main.171dd504.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

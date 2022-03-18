102f0e4fc0d95ff76ae8a3f27f1b32fdcf7ec75c
zamalovruslan <zamruma@gmail.com>
2022-03-19 00:56:44 +0300

    [feature] add eslint prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import 'modern-normalize/modern-normalize.css';
 import { HelloSection } from './components';
-import s from './App.module.css'
+import s from './App.module.css';
 
 function App() {
-  return (
-    <div className={s.app}>
-      <HelloSection />
-    </div>
-  );
+	return (
+		<div className={s.app}>
+			<HelloSection />
+		</div>
+	);
 }
 
 export default App;

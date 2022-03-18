ab3fb3501f042cb3e7203fa2a1c0368754a88f18
zamalovruslan <zamruma@gmail.com>
2022-03-19 01:09:11 +0300

    [feature] Прошелся претириром и eslint, поменял head заголовки

--- src/components/HelloSection/index.js
+++ src/components/HelloSection/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import s from './HelloSection.module.css';
 
 export const HelloSection = () => {
-  return (
-    <section className={s.section}>
-      <h1 className>Привет, меня зовут Руслан</h1>
-      <h3>Я разработчик на JavaScript</h3>
-      <p>На данный момент работаю в Совкомбанк Технологии</p>
-    </section>
-  )
-}
+	return (
+		<section className={s.section}>
+			<h1>Привет, меня зовут Руслан</h1>
+			<h3>Я разработчик на JavaScript</h3>
+			<p>На данный момент работаю в Совкомбанк Технологии</p>
+		</section>
+	);
+};

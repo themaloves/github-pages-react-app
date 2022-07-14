f44eea90c4feb6049070f5919379a5c7e8eea5cc
zamalovruslan <zamruma@gmail.com>
2022-07-15 00:48:52 +0300

    Project
    * Адаптивная версия меню и main контейнера

--- src/containers/NavContainer/components/Hamburger/index.tsx
+++ src/containers/NavContainer/components/Hamburger/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import className from 'classnames';
 import s from './Hamburger.module.scss';
 
-export function Hamburger({ handler }: { handler: () => void }) {
+type THamburgerProps = {
+	isOpenMenu: boolean;
+	setOpenMenu: (arg: boolean) => void;
+};
+
+export function Hamburger(
+	props: THamburgerProps
+): React.ReactElement {
+	const { isOpenMenu, setOpenMenu } = props;
+	const btnCls = className({
+		[s.menu]: true,
+		[s.opened]: isOpenMenu,
+	});
+
 	return (
-		<div className={s.section} onClick={handler}>
-			<span className={s.line} />
-		</div>
+		<button
+			className={btnCls}
+			onClick={() => setOpenMenu(!isOpenMenu)}
+		>
+			<svg width="30" height="30" viewBox="0 0 100 100">
+				<path
+					className={`${s.line} ${s.line1}`}
+					d="M 20,29.000046 H 80.000231 C 80.000231,29.000046
+					94.498839,28.817352 94.532987,66.711331 94.543142,77.980673
+					 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751
+					  75.000211,74.999942 75.000211,74.999942
+					   L 25.000021,25.000058"
+				/>
+				<path
+					className={`${s.line} ${s.line2}`}
+					d="M 20,50 H 80"
+				/>
+				<path
+					className={`${s.line} ${s.line3}`}
+					d="M 20,70.999954 H 80.000231 C 80.000231,70.999954
+					 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327
+					  90.966081,18.329754 85.259173,18.331003
+					   79.552261,18.332249
+					   75.000211,25.000058 75.000211,25.000058
+					    L 25.000021,74.999942"
+				/>
+			</svg>
+		</button>
 	);
 }
--- src/containers/NavContainer/index.tsx
+++ src/containers/NavContainer/index.tsx
@@ -1,38 +1,44 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import className from 'classnames';
+
 import { useMediaQuery } from 'hooks';
 import { Hamburger } from './components/Hamburger';
+
 import s from './NavContainer.module.scss';
 
 const links = [
 	{ key: 1, to: '/', name: 'Главная' },
 	{ key: 2, to: '/about', name: 'Обо мне' },
+	{ key: 3, to: '/articles', name: 'Статьи' },
+	{ key: 4, to: '/learn', name: 'Обучение' },
 ];
 
 export function NavContainer() {
 	const { pathname } = useLocation();
 	const matchesMedia = useMediaQuery('(max-width: 1200px)');
 	const [isVisible, setIsVisible] = useState(false);
-
-	function handlerVisibleMenu(value: boolean) {
-		setIsVisible(!value);
-	}
+	const menuCls = className({
+		[s.menu]: true,
+		[s.menuMedia]: matchesMedia,
+		[s.visible]: isVisible,
+	});
 
 	return (
 		<section className={s.section}>
 			{matchesMedia && (
 				<Hamburger
-					handler={() => handlerVisibleMenu(isVisible)}
+					isOpenMenu={isVisible}
+					setOpenMenu={setIsVisible}
 				/>
 			)}
-			<nav className={s.menu}>
-				<ul
-					className={`${s.items} ${isVisible && s.visible}`}
-				>
+			<nav className={menuCls}>
+				<ul className={`${s.items}`}>
 					{links.map(link => {
 						return (
 							<li key={link.key} className={s.item}>
 								<Link
+									onClick={() => setIsVisible(false)}
 									className={`${s.link} ${
 										pathname === link.to ? s.active : ''
 									}`}
